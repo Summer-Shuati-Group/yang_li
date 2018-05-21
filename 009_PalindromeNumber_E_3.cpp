13346238c5368d776170a5124f96147f720b5e27
ntsliyang <yli546@usc.edu>
2018-05-20 18:39:02 -0700

    009

--- 009_PalindromeNumber_E_3.cpp
+++ 009_PalindromeNumber_E_3.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-	/*
-	convert the original integer to a string
-	and compare if it's a palindrome string
-	*/
     bool isPalindrome(int x) {
+		/*
+		convert the original integer to a string
+		and compare if it's a palindrome string
+
+		Space: O(log10 n), where n is the size of the number
+		Time: O(log10 n), where n is the size of the number
+		*/
         if (x < 0) return false;
         string str = to_string(x);
         for (int i = 0; i < str.length() / 2; i++) {
@@ -13,5 +16,29 @@ public:
             }
         }
         return true;
+
+
+		/*
+		RULING OUT TWO OPTIONS: 
+		1. negative numbers, negative sign would appear at the end of the number if reverted
+		2. non-zero number with zero at its end, zero would appear at the front at the
+		front if reverted. However, any non-zero does not have such property
+
+		THEN: 
+		if it is even, compare the first half of the number and second half of the number
+		if it is odd, do the same thing and middle digit would always equal to itself 
+		(by dividing 10, we can rule out the middle digit)
+
+		Space: O(1)
+		Time: O(log10 n), where n is the size of the number
+		*/
+		if (x < 0 || (x%10 == 0 && x != 0)) return false;
+        int y = 0;
+        while (x > y) {
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return x==y || x == y / 10;
     }
 };
