cddef9d5d14b0c22815e6dec960cd9f22fb96903
ntsliyang <yli546@usc.edu>
2018-06-10 22:29:37 -0700

    150

--- 150_EvaluateReversePolishNotation_M_23.cpp
+++ 150_EvaluateReversePolishNotation_M_23.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
+		// 1. iterative implementation: 
         stack<int> ans;
         int sum = 0;
         for (int i = 0; i < tokens.size(); i++) {
@@ -31,5 +32,19 @@ public:
 			// do the corresponding operation between two top numbers
         }
         return ans.top();
+
+		// 2. recursive implementation: 
+		// 		(1). pop string from the end of the vector
+		// 		(2). if it's number, just return it
+		// 		(3). if it's operation, call function recursively for 2nd operand and 1st
+		string s = tokens.back(); tokens.pop_back();
+        if(s != "+" && s != "-" && s != "*" && s != "/") return stoi(s);
+        
+        int r2 = evalRPN(tokens), r1 = evalRPN(tokens);
+        
+        if(s == "+") return r1 + r2;
+        if(s == "-") return r1 - r2;
+        if(s == "*") return r1 * r2;
+        if(s == "/") return r1 / r2;
     }
 };
